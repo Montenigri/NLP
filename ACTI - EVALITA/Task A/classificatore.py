79d9587ae7c587cb2ac35521c4d8bac028457c76
Montenigri <gabrielemusso@live.it>
2023-12-19 11:35:23 +0100

    .

--- ACTI - EVALITA/Task A/classificatore.py
+++ ACTI - EVALITA/Task A/classificatore.py
@@ -7,15 +7,14 @@ from torch.optim import Adam
 from torch import nn
 import torch
 from sklearn.model_selection import train_test_split
-from transformers import AutoConfig, AutoModel, AutoTokenizer, get_linear_schedule_with_warmup
+from transformers import AutoConfig, AutoModel, AutoTokenizer, AutoModelForSequenceClassification
 from sklearn.utils import shuffle
-from sklearn.metrics import accuracy_score, classification_report
 import nltk
 import matplotlib.pyplot as plt
 import emoji
-
-pd.set_option('display.max_colwidth', None)
-
+dataset = pd.read_csv('Dataset/subtaskA_train.csv',  header=0, names=['id', 'text', 'label'])
+augmented_dataset = dataset.copy()
+augmented_dataset = augmented_dataset['text'].apply(lambda x: x.replace('\r', ' ').replace('\n', ' '))
 def process_data_augmentation(dataset):
     typeA=['[Sinonimi]:','[Riassunto]:','[Tono diverso]:','[Parole chiave sostituite]:','[Domanda diretta]:']
     typeB=['Riscrivi il testo originale utilizzando sinonimi per esprimere lo stesso significato in italiano:','Fornisci un riassunto del testo originale in italiano:','Riscrivi il testo originale con un tono diverso, mantenendo però il medesimo significato in italiano:', 'Sostituisci alcune parole chiave nel testo originale in italiano:','Trasforma il testo originale in una domanda diretta in italiano:']
@@ -25,34 +24,43 @@ def process_data_augmentation(dataset):
     #dataset['augmented_text'] = dataset['augmented_text'].apply(lambda x: x.replace('\r', ' ').replace('\n', ' '))
 
     for i in tqdm(range(len(dataset))):
+        #Copio la singola riga del dataset in una variabile per usarla come stringa
+        
         augmented = dataset.loc[i,'augmented_text']
+        #Divido la stringa per riga
         augmented = augmented.split('\n')
-
+        #Per ogni riga controllo se è presente una delle stringhe di tipo A o B
+        #Esistono due tipi poiché i due modelli restituivano due strutture leggermente diverse
         for statement in augmented:
+            #Verifico la riga appartenga al risultato e taglio la parte introduttiva
             for start in typeA:
                 if start in statement:
                     augmented_dataset.loc[0,'text'] = statement.replace(start, '')
                     augmented_dataset.loc[0,'label'] = dataset.loc[i,'label']
+                    #Aggiungo la riga al dataset
                     processed_dataset = pd.concat([processed_dataset,augmented_dataset],ignore_index=True)
             for start in typeB:
                 if start in statement:
                     augmented_dataset.loc[0,'text'] = statement.replace(start, '')
                     augmented_dataset.loc[0,'label'] = dataset.loc[i,'label']
                     processed_dataset = pd.concat([processed_dataset,augmented_dataset],ignore_index=True)
-
+                    
     processed_dataset['text'] = processed_dataset['text'].apply(lambda x: x.replace('\r', ' ').replace('\n', ' '))
 
     return processed_dataset
+
 augmented_dataset = pd.read_csv('Dataset/augmented_dataset1.csv',  header=0, names=['text', 'label','augmented_text'])
 
 augmented_dataset = process_data_augmentation(augmented_dataset)
+
+
+
+
 augmented_dataset['char_count'] = augmented_dataset['text'].str.len()
-
-
-
+augmented_dataset = shuffle(augmented_dataset, random_state=42)
 import re
 def add_emoji(dataset):
-
+    
     augmented_dataset = dataset.copy()
     augmented_dataset['emoji'] = ' '
 
@@ -64,42 +72,29 @@ def add_emoji(dataset):
         emoji_found = re.findall(pattern,data)
         emoji_found = ' '.join(emoji_found)
         augmented_dataset.at[i,'emoji'] = emoji_found
-
+        
     return augmented_dataset
 
 augmented_dataset = add_emoji(augmented_dataset)
-
 def add_hashtag(dataset):
     augmented_dataset = dataset.copy()
     augmented_dataset['hashtag'] = ' '
     data = []
-
+    
     for i in tqdm(range(len(dataset)), desc= "Looking for hashtag"):
         text = dataset.loc[i,'text']
         dato = [k for k in text.split() if k.startswith("#")]
         data.append(dato)
-
+        
     augmented_dataset['hashtag'] = data
     augmented_dataset['hashtag'] = augmented_dataset['hashtag'].apply(lambda x: " ".join(x).replace('#', ''))
-
+  
 
     return augmented_dataset
-
 augmented_dataset = add_hashtag(augmented_dataset)
-
-
-augmented_dataset = shuffle(augmented_dataset, random_state=42)
-
-augmented_dataset.to_csv('Dataset/augmented_dataset_with_added_cols.csv', index=False)
-
-
-######
-
-
-
 hyperparameters = {
-    "#_classes" : 2,
-    "epochs": 10,
+    "#_classes" : 1, #Si imposta come valore numero classi -1 per via dell'inizio dell'indice a 0
+    "epochs": 5,
     "learning_rate": 1e-5,
     "batch_size": 8,
     "dropout": 0.1,
@@ -111,10 +106,10 @@ hyperparameters = {
     "extra_features": 65, #32 emoji + 32 hashtag + 1 char_count
 }
 
+
 (x_train, x_test,char_count_train,char_count_test, hashtag_train, hashtag_test,emoji_train,emoji_test,y_train, y_test) = train_test_split(augmented_dataset['text'],augmented_dataset['char_count'], augmented_dataset['hashtag'],augmented_dataset['emoji'] ,augmented_dataset['label'], test_size=0.2, random_state=42)
 
 (x_train, x_val,char_count_train,char_count_val, hashtag_train, hashtag_val, emoji_train, emoji_val, y_train, y_val) = train_test_split( x_train,char_count_train,hashtag_train,emoji_train, y_train, test_size=0.1, random_state=42)
-
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -123,14 +118,14 @@ class Dataset(torch.utils.data.Dataset):
 
     def __init__(self, x, char_count, emoji, hashtag, y, stopwords):
 
-
+        
         tokens_litt = [nltk.word_tokenize(text, language='italian') for text in list(x)]
-
+       
         text_clean = []
-
+        
         for sentence in tqdm(tokens_litt, desc='Tokenizing text ... '):
             text_clean.append(' '.join([w for w in sentence if not w.lower() in nltk.corpus.stopwords.words("italian")]))
-
+        
 
         self.texts = [text for text in text_clean]
         self.emoji = [e for e in emoji]
@@ -140,7 +135,7 @@ class Dataset(torch.utils.data.Dataset):
 
     def classes(self):
         return self.labels
-
+    
     def __len__(self):
         return len(self.labels)
 
@@ -159,11 +154,11 @@ class Dataset(torch.utils.data.Dataset):
     def get_batch_emoji(self, idx):
         # Fetch a batch of inputs
         return self.emoji[idx]
-
+    
     def get_batch_hashtag(self, idx):
         # Fetch a batch of inputs
         return self.hashtag[idx]
-
+    
     def __getitem__(self, idx):
 
         batch_texts = self.get_batch_texts(idx)
@@ -173,49 +168,11 @@ class Dataset(torch.utils.data.Dataset):
         batch_hashtag = self.get_batch_hashtag(idx)
 
         return batch_texts, batch_labels, batch_char_count, batch_emoji, batch_hashtag
-
+    
 train_dataset = Dataset(x_train, char_count_train,emoji_train,hashtag_train, y_train, hyperparameters["stopwords"])
 val_dataset = Dataset(x_val,char_count_val,emoji_val,hashtag_val, y_val, hyperparameters["stopwords"])
 test_dataset = Dataset(x_test, char_count_test,emoji_test,hashtag_test, y_test, hyperparameters["stopwords"])
 
-'''
-class ClassifierDeep(nn.Module):
-
-    def __init__(self, labels, hdim, dropout, model_name,extra_features = hyperparameters['extra_features']):
-        super(ClassifierDeep, self).__init__()
-        config = AutoConfig.from_pretrained(model_name)
-        self.lm_model = AutoModel.from_pretrained(model_name, config=config)
-        self.classifier = nn.Sequential(
-            nn.Dropout(dropout),
-            nn.Linear(hdim + extra_features, 64),
-            nn.ReLU(),
-            nn.Linear(64, labels),
-            )
-
-    def forward(self, input_id_text, attention_mask, char_count,emoji_text, hashtag):
-        output = self.lm_model(input_id_text, attention_mask).last_hidden_state
-        output = output[:,0,:]
-        output = torch.cat((output, char_count.unsqueeze(-1),emoji_text,hashtag), dim=1)  # Concatena il conteggio dei caratteri
-        return self.classifier(output)  
-'''
-class ClassifierDeep(nn.Module):
-
-    def __init__(self, labels, hdim, dropout, model_name,extra_features = hyperparameters['extra_features']):
-        super(ClassifierDeep, self).__init__()
-        config = AutoConfig.from_pretrained(model_name)
-        self.lm_model = AutoModel.from_pretrained(model_name, config=config)
-        self.classifier = nn.Sequential(
-            nn.Dropout(dropout),
-            nn.ReLU(),
-            nn.Linear(hdim + extra_features, 2)
-            )
-
-    def forward(self, input_id_text, attention_mask, char_count,emoji_text, hashtag):
-        output = self.lm_model(input_id_text, attention_mask).last_hidden_state
-        output = output[:,0,:]
-        output = torch.cat((output, char_count.unsqueeze(-1),emoji_text,hashtag), dim=1)  # Concatena il conteggio dei caratteri
-        return self.classifier(output)
-    
 class EarlyStopping:
     def __init__(self, patience=5, min_delta=0.0):
 
@@ -238,7 +195,24 @@ class EarlyStopping:
                                                                             # come nuova loss ottimale
             self.counter = 0                                                # e azzeriamo il contatore di pazienza
 
+class ClassifierDeep(nn.Module):
 
+    def __init__(self, labels, hdim, dropout, model_name,extra_features = hyperparameters['extra_features']):
+        super(ClassifierDeep, self).__init__()
+        config = AutoConfig.from_pretrained(model_name)
+        self.lm_model = AutoModel.from_pretrained(model_name, config=config)
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout),
+            nn.Linear(hdim + extra_features, 64),
+            nn.Linear(64, labels),
+            nn.Sigmoid()
+            )
+
+    def forward(self, input_id_text, attention_mask, char_count,emoji_text, hashtag):
+        output = self.lm_model(input_id_text, attention_mask).last_hidden_state
+        output = output[:,0,:]
+        output = torch.cat((output, char_count.unsqueeze(-1), emoji_text, hashtag), dim=1)  # Concatena il conteggio dei caratteri
+        return self.classifier(output)
 
 def train_loop(model, dataloader, tokenizer, loss, optimizer, device):
     model.train()
@@ -273,12 +247,10 @@ def train_loop(model, dataloader, tokenizer, loss, optimizer, device):
         batch_labels = batch_labels.to(device)
         batch_char_count = batch_char_count.to(device)
 
-        
         output = model(input_id_texts, mask_texts,batch_char_count, input_id_emoji, input_id_hashtag)
 
-
         # la loss è una CrossEntropyLoss, al suo interno ha la logsoftmax + negative log likelihood loss
-        batch_loss = loss(output, batch_labels)
+        batch_loss = loss(output, batch_labels.unsqueeze(-1).float())
         batch_loss.backward()
         optimizer.step()
 
@@ -296,7 +268,6 @@ def train_loop(model, dataloader, tokenizer, loss, optimizer, device):
 
 
     return epoch_loss/len(dataloader), epoch_acc
-
 
 def test_loop(model, dataloader, tokenizer, loss, device):
     model.eval()
@@ -332,7 +303,7 @@ def test_loop(model, dataloader, tokenizer, loss, device):
             batch_char_count = batch_char_count.to(device)
             output = model(input_id_texts, mask_texts,batch_char_count,input_id_emoji, input_id_hashtag)
 
-            batch_loss = loss(output, batch_labels)
+            batch_loss = loss(output, batch_labels.unsqueeze(-1).float())
             epoch_loss += batch_loss.item()
 
             softmax = nn.LogSoftmax(dim=1)
@@ -345,6 +316,7 @@ def test_loop(model, dataloader, tokenizer, loss, device):
             batch_char_count = batch_char_count.detach().cpu()
 
     return epoch_loss/len(dataloader), epoch_acc
+
 
 def train_test(model, epochs, optimizer, device, train_data, test_data,
                batch_size, language_model, train_loss_fn, test_loss_fn=None,
@@ -390,14 +362,10 @@ def train_test(model, epochs, optimizer, device, train_data, test_data,
         test_acc.append(epoch_test_acc/len(test_data))
 
         val_loss_str = f'Validation loss: {epoch_validate_loss:6.4f} ' if early_stopping != None else ' '
-                        # ' if early_stopping != None else ' '
         val_acc_str = f'Validation accuracy: {(epoch_validate_acc/len(val_data)):6.4f} ' if early_stopping != None else ' '
-                        # ' if early_stopping != None else ' '
         print(f"\nTrain loss: {epoch_train_loss:6.4f} {val_loss_str}Test loss: {epoch_test_loss:6.4f}")
-                        # Test loss: {epoch_test_loss:6.4f}")
         print(f"Train accuracy: {(epoch_train_acc/len(train_data)):6.4f} {val_acc_str}Test accuracy: {(epoch_test_acc/len(test_data)):6.4f}")
-                        # {val_acc_str}Test accuracy:
-                        # {(epoch_test_acc/len(test_data)):6.4f}")
+                        
 
         # early stopping
         if early_stopping != None:
@@ -407,7 +375,6 @@ def train_test(model, epochs, optimizer, device, train_data, test_data,
 
     return train_loss, validation_loss, test_loss, train_acc, validation_acc, test_acc
                         # train_acc, validation_acc, test_acc
-
 # Acquisiamo il device su cui effettueremo il training
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using {device} device")
@@ -421,18 +388,18 @@ print(model)
 # Calcoliamo il numero totale dei parametri del modello
 total_params = sum(p.numel() for p in model.parameters())
 print(f"Numbero totale dei parametri: {total_params}")
-
-criterion = nn.CrossEntropyLoss()
+#La funzione di loss va modificata in base al parametro #_classes
+criterion = nn.BCELoss()
 optimizer = Adam(model.parameters(), lr=hyperparameters["learning_rate"])
 
 # Creiamo la callback di early stopping da passare al nostro metodo di addestramento
 early_stopping = EarlyStopping(patience=hyperparameters['patience'],
                                min_delta=hyperparameters['min_delta'])
 
-train_loss, validation_loss,test_loss, train_acc, validation_acc, test_acc = train_test(model,
-                                                # train_test(model,
+
+train_loss, validation_loss,test_loss, train_acc, validation_acc, test_acc = train_test(
+                                                model,
                                                 hyperparameters['epochs'],
-                                                #50,
                                                 optimizer,
                                                 device,
                                                 train_dataset,
@@ -444,10 +411,66 @@ train_loss, validation_loss,test_loss, train_acc, validation_acc, test_acc = tra
                                                 early_stopping,
                                                 val_dataset)
 
+import pickle
+with open('train_loss_aug.pickle', 'wb') as f:
+    pickle.dump(train_loss, f)
+with open('validation_loss_aug.pickle', 'wb') as f:
+    pickle.dump(validation_loss, f)
+with open('test_loss_aug.pickle', 'wb') as f:
+    pickle.dump(test_loss, f)
+with open('train_acc_aug.pickle', 'wb') as f:
+    pickle.dump(train_acc, f)
+with open('validation_acc_aug.pickle', 'wb') as f:
+    pickle.dump(validation_acc, f)
+with open('test_acc_aug.pickle', 'wb') as f:
+    pickle.dump(test_acc, f)
 
-torch.save({
-            'epoch': 10, #Epoca finale, da modificare in modo da renderla dinamica
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': train_loss,
-            }, "model_10_epochs")
+#################
+###############
+########            Dataset senza augmentation  
+###############
+#################
+dataset = pd.read_csv('Dataset/subtaskA_train.csv',  header=0, names=['id', 'text', 'label'])
+dataset['text'] = dataset['text'].apply(lambda x: x.replace('\r', ' ').replace('\n', ' '))
+dataset['char_count'] = dataset['text'].str.len()
+dataset = add_emoji(dataset)
+dataset = add_hashtag(dataset)
+dataset.drop(columns=['id'], inplace=True)
+
+
+(x_train_original_dataset, x_test_original_dataset,char_count_train_original_dataset,char_count_test_original_dataset, hashtag_train_original_dataset, hashtag_test_original_dataset,emoji_train_original_dataset,emoji_test_original_dataset,y_train_original_dataset, y_test_original_dataset) = train_test_split(dataset['text'],dataset['char_count'], dataset['hashtag'],dataset['emoji'] ,dataset['label'], test_size=0.2, random_state=42)
+
+(x_train_original_dataset, x_val_original_dataset,char_count_train_original_dataset,char_count_val_original_dataset, hashtag_train_original_dataset, hashtag_val_original_dataset, emoji_train_original_dataset, emoji_val_original_dataset, y_train_original_dataset, y_val_original_dataset) = train_test_split( x_train_original_dataset,char_count_train_original_dataset,hashtag_train_original_dataset,emoji_train_original_dataset, y_train_original_dataset, test_size=0.1, random_state=42)
+
+train_dataset_original_dataset = Dataset(x_train_original_dataset, char_count_train_original_dataset,emoji_train_original_dataset,hashtag_train_original_dataset, y_train_original_dataset, hyperparameters["stopwords"])
+val_dataset_original_dataset = Dataset(x_val_original_dataset,char_count_val_original_dataset,emoji_val_original_dataset,hashtag_val_original_dataset, y_val_original_dataset, hyperparameters["stopwords"])
+test_dataset_original_dataset = Dataset(x_test_original_dataset, char_count_test_original_dataset,emoji_test_original_dataset,hashtag_test_original_dataset, y_test_original_dataset, hyperparameters["stopwords"])
+
+
+
+train_loss, validation_loss,test_loss, train_acc, validation_acc, test_acc = train_test(
+                                                model,
+                                                hyperparameters['epochs'],
+                                                optimizer,
+                                                device,
+                                                train_dataset_original_dataset,
+                                                test_dataset_original_dataset,
+                                                hyperparameters['batch_size'],
+                                                hyperparameters['language_model'],
+                                                criterion,
+                                                criterion,
+                                                early_stopping,
+                                                val_dataset_original_dataset)
+
+with open('train_loss.pickle', 'wb') as f:
+    pickle.dump(train_loss, f)
+with open('validation_loss.pickle', 'wb') as f:
+    pickle.dump(validation_loss, f)
+with open('test_loss.pickle', 'wb') as f:
+    pickle.dump(test_loss, f)
+with open('train_acc.pickle', 'wb') as f:
+    pickle.dump(train_acc, f)
+with open('validation_acc.pickle', 'wb') as f:
+    pickle.dump(validation_acc, f)
+with open('test_acc.pickle', 'wb') as f:
+    pickle.dump(test_acc, f)
